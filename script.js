ed28fc96de45392069847d05811361172cfc6ce0
LeoneWelch <leonelaghmich88@gmail.com>
2025-04-25 23:50:50 +0200

    je commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        document.body.classList.add("loaded");
+    }, 3000); // Correspond à la durée de l'animation de saisie
+});
